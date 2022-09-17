4a3553983dc7359661a478574970ee7c68146f65
collinr3 <collinr3@btinternet.com>
2022-09-17 18:41:08 +0100

    Added filter and search to Admin ------------- Ran 29 tests in 7.202s
    OK

--- django_dbq/admin.py
+++ django_dbq/admin.py
@@ -5,4 +5,7 @@ from .models import Job
 
 @admin.register(Job)
 class JobAdmin(admin.ModelAdmin):
+
     list_display = ('id', 'name', 'state', 'queue_name', 'priority', 'run_after')
+    list_filter =  ('name', 'state', 'queue_name', 'priority')
+    search_fields = ('id', 'run_after')
